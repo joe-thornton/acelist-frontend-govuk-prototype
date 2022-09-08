5bf31e61fd0da1d8aa523cb3f4ae5d5053e2d1f9
Joe Thornton <thornton.jp@gmail.com>
2022-09-08 17:03:13 +0100

    updated submission page to post to backend API to create an advert in the database

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,28 @@
 const express = require('express')
 const router = express.Router()
+const axios = require('axios')
 
 // Add your routes here - above the module.exports line
+router.post('/submit-item', (req, res) => {
+//    console.log('in this route')
+//    console.log(req)
+    console.log(req.body)
+//    req.on('end', (data) => {
+//    console.log('data has arrived')
+//    })
+    const {Title, Category, Description, Price} = req.body
+
+    axios.post('http://localhost:9000/adverts', {
+        title: Title,
+        body: Description,
+        subBoard: Category,
+        price: Number(Price)
+        }
+    )
+    .then(APIresponse => res.redirect('/confirmation'))
+    .catch((error) => {console.log(error)})
+
+})
 
 module.exports = router
+
